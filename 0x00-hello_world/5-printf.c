f79ea823e1896f885da4ebd7209457b76b37eb11
Sarbeng <sarbengbaafi@gmail.com>
2022-09-08 14:55:09 -0700

    using printf to print out a string

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Emtry point
+ *
+ * printf - will be used to print out  a string
+ *
+ * Return: Always 0 (success)
+ *
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
