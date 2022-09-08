c1eaa3846f8cdec80329369ba176ad02dc55af3d
Sarbeng <sarbengbaafi@gmail.com>
2022-09-08 14:30:43 -0700

    errors from betty linter fixed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
 /**
- * Starting the code inside the main function
+ * main - Starting the code inside the main function
  *
  * Printing out "Programming is like building a multilingual puzzle"
  *
  * and returning 0 as success
+ * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
