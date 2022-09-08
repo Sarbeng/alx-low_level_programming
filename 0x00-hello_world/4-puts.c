78afe494b9f0ba1691ba539985404355b22b61e8
Sarbeng <sarbengbaafi@gmail.com>
2022-09-08 14:10:41 -0700

    a program that uses puts to print a line and move to the next line

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * Starting the code inside the main function
+ *
+ * Printing out "Programming is like building a multilingual puzzle"
+ *
+ * and returning 0 as success
+ */
+int main (void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return 0;
+}
