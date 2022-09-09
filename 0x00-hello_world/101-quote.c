fb0a5c619623ee6e0c17a227efe681c2ad12a6fa
Sarbeng <sarbengbaafi@gmail.com>
2022-09-08 17:27:24 -0700

    Added quote c file

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * printf - cos i can
+ *
+ * Return: Always  0 (success)
+ */
+int main(void)iiiii
+{
+	char s[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	fprintf(stdout, "%s\n", s);
+	return (0);
+}
