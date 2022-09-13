322fc564cef22de76c284888289fe569aa7291e9
Sarbeng <sarbengbaafi@gmail.com>
2022-09-13 14:23:10 -0700

    fixed chracter exceeded issues with checker i think

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,7 +8,7 @@ void print_alphabet_x10(void)
 	char letter_x10;
 	int count;
 
-	for (count = 0; count <= 10; count++)
+	for (count = 0; count <= 9; count++)
 	{
 		/* second for loop to print the values*/
 		for (letter_x10 = 'a'; letter_x10 <= 'z'; letter_x10++)
