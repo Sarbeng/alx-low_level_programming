92407632b77a476a6062f18d2465dba2f365ce9e
Sarbeng <sarbengbaafi@gmail.com>
2022-09-15 06:00:41 -0700

    function that checks for a digit (0 through 9).

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -11,6 +11,12 @@
 int _isupper(int c);
 
 /**
+ * isdigit - checks if its digit
+ * for digits 0 to 9
+ */
+int _isdigit(int c);
+
+/**
  * mul - multiplies two numbers
  */
 int mul(int a, int b);
