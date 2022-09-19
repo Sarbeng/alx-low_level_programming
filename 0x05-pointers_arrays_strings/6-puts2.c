37ec5679f579f15fd496e15835becbc0d836c618
Sarbeng <sarbengbaafi@gmail.com>
2022-09-19 13:18:22 -0700

    Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * prints every other character of a string
+ * starting with the first character
+ *
+ * @str: pointer to string
+ */
+void puts2(char *str)
+{
+	int i;
+
+	i = 0;
+	/**
+	 * use while loop to print string
+	 */
+	while ( *(str + i) != '\0')
+	{
+		/**
+		 * print every other character
+		 * starting from first
+		 */
+		if (i % 2 == 0)
+		{
+			_putchar(*(str + i));
+		}
+		/**
+		 * to prevent an infinite loop
+		 */
+		i++;
+	}
+	_putchar('\n');
+}
