28afbf157a8b33f7502ecf5e94b4bd653e0076d2
Sarbeng <sarbengbaafi@gmail.com>
2022-09-19 07:58:01 -0700

    all prototypes added to main

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -23,4 +23,44 @@ void swap_int(int *a, int *b);
  */
 int _strlen(char *s);
 
+/**
+ * function that prints a string, followed by a new line, to stdout.
+ */
+void _puts(char *str);
+
+/**
+ * function that prints a string, in reverse, followed by a new line.
+ */
+void print_rev(char *s);
+
+/**
+ * function that reverses a string.
+ */
+void rev_string(char *s);
+
+/**
+ * function that prints every other character of a string,
+ *  starting with the first character, followed by a new line.
+ */
+void puts2(char *str);
+
+/**
+ * function that prints half of a string, followed by a new line.
+ */
+void puts_half(char *str);
+
+/**
+ *  function that prints n elements of an array of integers
+ *   followed by a new line
+ */
+void print_array(int *a, int n);
+
+/**
+ *  function that copies the string pointed to by src
+ *  including the terminating null byte (\0)
+ *   to the buffer pointed to by dest.
+ *   Return value: the pointer to dest
+ */
+char *_strcpy(char *dest, char *src);
+
 #endif /* _MAIN_H_ */
