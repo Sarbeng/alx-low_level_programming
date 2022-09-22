9149b849481380a4f6df7c1228066624763e6396
Sarbeng <sarbengbaafi@gmail.com>
2022-09-22 03:30:04 -0700

    lower to upper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,10 +1,14 @@
 #include "main.h"
 /**
- * *string_toupper - changes all lowercase of a string 
+ * *string_toupper - changes all lowercase of a string
  * to upper
- * it takes all pointer values 
+ * it takes all pointer values
  * and returns all lowercase to upper by subtracting the ascii
  * by 32
+ *
+ * @s: string pointer to change to upper
+ *
+ * Return: pointer to string
  */
 char *string_toupper(char *s)
 {
