d3abdfde517562d1792602f077176ef5eb46f540
Kwadwo Sarbeng-Baafi <35800995+Sarbeng@users.noreply.github.com>
2022-09-30 16:33:57 +0000

    Delete C directory

--- C/0x0A-argc_argv/1-args.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-/**
- * main - will print its name followed by new line
- * Return: no return value
- */
-int main(int argc, char *argv[])
-{
-    printf("%d\n",argc - 1);
-}
--- C/0x0A-argc_argv/main.h
+++
@@ -1,4 +0,0 @@
-/**
- * prints items to the screen, char by char
- */
-int _putchar(char c);
