8c6d743ff9d30394f0336da311935ff3a46cd147
Sarbeng <sarbengbaafi@gmail.com>
2022-10-20 08:24:41 -0700

    Add node

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+* print_before - prints string before main function
+* Return: no return
+*/
+void print_before(void)
+{
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
+}
